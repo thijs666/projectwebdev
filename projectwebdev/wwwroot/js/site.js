30fdd73322d23c72db2ccd61b9bf62c3db78c7f5
Thijs Zijlstra <thijszijlstra@icloud.com>
2022-03-19 14:55:33 +0100

    Restyle Sidebar and Header

--- projectwebdev/wwwroot/js/site.js
+++ projectwebdev/wwwroot/js/site.js
@@ -2,3 +2,29 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    
+    const showNavbar = (toggleId, navId, bodyId, headerId) => {
+        const toggle = document.getElementById(toggleId),
+            nav = document.getElementById(navId),
+            bodypd = document.getElementById(bodyId),
+            headerpd = document.getElementById(headerId)
+
+        // Validate that all variables exist
+        if (toggle && nav && bodypd && headerpd) {
+            toggle.addEventListener('click', () => {
+                // show navbar
+                nav.classList.toggle('show')
+                // change icon
+                toggle.classList.toggle('bi-x-lg')
+                // add padding to body
+                bodypd.classList.toggle('body-pd')
+                // add padding to header
+                headerpd.classList.toggle('body-pd')
+            })
+        }
+    }
+
+    showNavbar('sidebar_toggle', 'nav-bar', 'body-pd', 'header')
+});
