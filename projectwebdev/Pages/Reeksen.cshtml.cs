711165361e4daa4c11f165b747eadabda523f652
Thijs Zijlstra <thijszijlstra@icloud.com>
2022-03-19 11:20:16 +0100

    Add Top Layer Pages

---
+++ projectwebdev/Pages/Reeksen.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace projectwebdev.Pages
+{
+    public class ReeksenModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ projectwebdev/Pages/Titels.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace projectwebdev.Pages
+{
+    public class TitelsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
