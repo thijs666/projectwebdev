0ab1d3df8b1e25781ac07a5db7e4dc7f138882ca
Thijs Zijlstra <thijszijlstra@icloud.com>
2022-03-19 11:22:00 +0100

    Require Login

--- projectwebdev/Pages/Uitgevers.cshtml.cs
+++ projectwebdev/Pages/Uitgevers.cshtml.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace projectwebdev.Pages
 {
+    [Authorize]
     public class UitgeversModel : PageModel
     {
         public void OnGet()
