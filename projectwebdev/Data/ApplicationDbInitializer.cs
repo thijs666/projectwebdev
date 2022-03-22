e04b7a1e94350b3cd5ad23c97531aba9d669c35e
Thijs Zijlstra <thijszijlstra@icloud.com>
2022-03-22 17:53:26 +0100

    Add Seeding Comments

--- projectwebdev/Data/ApplicationDbInitializer.cs
+++ projectwebdev/Data/ApplicationDbInitializer.cs
@@ -13,23 +13,29 @@ namespace projectwebdev.Data
         
         public void Seed()
         {
+            // Admininistrator Role ID
             const string adminRoleId = "2301D884-221A-4E7D-B509-0113DCC043E1";
 
+            // Create the Administrator Role
             builder.Entity<IdentityRole>().HasData(
                 new IdentityRole() { Id = adminRoleId, Name = "Administrator", NormalizedName = "ADMINISTRATOR" }
             );
 
+            // Basic Admin User ID
             const string adminUserId = "B22698B8-42A2-4115-9631-1C2D1E2AC5F7";
+
+            // Hasher for creation of basic admin user password
             var hasher = new PasswordHasher<IdentityUser>();
 
             builder.Entity<IdentityUser>().HasData(
-                // admin user
+                // create admin user
                 new IdentityUser() { Id = adminUserId, UserName = "admin@localhost", NormalizedUserName = "ADMIN@LOCALHOST", Email = "admin@localhost", NormalizedEmail = "ADMIN@LOCALHOST", PasswordHash = hasher.HashPassword(null, "root"), EmailConfirmed = true },
                 
-                // registered user
+                // for testing - registered user
                 new IdentityUser() { UserName = "user@localhost", NormalizedUserName = "USER@LOCALHOST", Email = "user@localhost", NormalizedEmail = "USER@LOCALHOST", PasswordHash = hasher.HashPassword(null, "root"), EmailConfirmed = true }
             );
 
+            // Give the Basic admin user the Administrator Role
             builder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
             {
                 RoleId = adminRoleId,
