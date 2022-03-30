eda6c505b098512eb2808cc5ea62a2726ac6fb81
Thijs Zijlstra <thijszijlstra@icloud.com>
2022-03-30 10:11:11 +0200

    Disable email confirmation

--- projectwebdev/Program.cs
+++ projectwebdev/Program.cs
@@ -20,7 +20,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseMySql(connectionString, serverVersion));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
